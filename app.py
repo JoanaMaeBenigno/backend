c4701e344b8cef425d0c2a2ade5652d53a771f6d
saniro <jairo.sanchez943.js@gmail.com>
2025-04-18 15:33:09 +0800

    Added database connection

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from flask import Flask
-from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 import os
 
-load_dotenv()  # Load .env into environment variables
+from src.config.config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
     return "Hello, Flask!"
+
+@app.route('/db-check')
+def db_check():
+    try:
+        # Executes a simple query
+        db.session.execute(text('SELECT 1'))
+        return "✅ PostgreSQL connected!"
+    except Exception as e:
+        return f"❌ Connection failed: {str(e)}"
 
 if __name__ == '__main__':
     debug = os.getenv("DEBUG", "False").lower() == "true"
